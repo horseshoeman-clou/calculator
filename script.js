22a122ad60be08db78579cf764a242f49a7c7de8
horseshoeman-clou <218686597+horseshoeman-clou@users.noreply.github.com>
2026-07-12 18:18:00 +0530

    Add clear button handling and first/second number + operator storage

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('.row button');
 
+let firstNumber ="";
+let secondNumber ="";
+let operator ="";
+
 buttons.forEach(button =>{
     button.addEventListener('click',()=> {
+
+
+
+        if(button.classList.contains('clear-btn')){
+            display.innerText = '0';
+            firstNumber="";
+            secondNumber="";
+            operator="";
+            return;
+        }
+
+        if(button.innerText === "+" || button.innerText === "-" || button.innerText === "/" || button.innerText === "x"){
+            operator = button.innerText;
+            display.innerText+=operator;
+            return;
+        }
+
+        if(operator ===""){
+            firstNumber+=button.innerText;
+        }
+        else{
+            secondNumber+=button.innerText;
+        }
         
         if(display.innerText === '0'){
             display.innerText = button.innerText;
@@ -11,5 +38,7 @@ buttons.forEach(button =>{
         else{
             display.innerText += button.innerText;
         }
+
+        
     });
 });
